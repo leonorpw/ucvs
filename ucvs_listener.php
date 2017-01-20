5cf68c3c3782ab946b24d19a8aafac84df2a0311
lemoniscool <contact@lemoniscool.pw>
2017-01-21 00:02:50 +0100

    use new log class

--- ucvs_listener.php
+++ ucvs_listener.php
@@ -1,61 +1,60 @@
 <?php
-/***************************************/
-/* UCVS - Unified Callback Vote Script */
-/*		Written by LemoniscooL		   */
-/*	 released under GPLv3 license	   */
-/***************************************/
+	/***************************************/
+	/* UCVS - Unified Callback Vote Script */
+	/*		Written by LemoniscooL		   */
+	/*	 released under GPLv3 license	   */
+	/***************************************/
 
 
-/*=================================================================================*/
-/* Debug settings																   */
-/* These settings are solely for debug purposes, dont change anything here!		   */
-/*=================================================================================*/
+	/*=================================================================================*/
+	/* Debug settings																   */
+	/* These settings are solely for debug purposes, dont change anything here!		   */
+	/*=================================================================================*/
 
-//Uncomment to show all errors except notice and warnings
-error_reporting(E_ALL);
+	//Uncomment to show all errors
+	error_reporting(E_ALL);
 
-//in case no errors are shown uncomment this
-ini_set("display_errors", 1);
+	//in case no errors are shown uncomment this
+	ini_set("display_errors", 1);
 
-require_once("classes/ucvs.class.php");
+	require_once("classes/ucvs.class.php");
 
-$ucvs = new ucvsCore();
+	$ucvs = new ucvsCore();
 
-//Check wich request method was used and create a reference
-switch($_SERVER['REQUEST_METHOD'])
-{
-	case 'GET':
-		$the_request = &$_GET;
-	break;
-	
-	case 'POST':
-		$the_request = &$_POST;
-	break;
-}
-
-//store requesting IP in a variable | compatibility with CloudFlare
-$reqIP = isset($_SERVER['HTTP_CF_CONNECTING_IP']) ? $_SERVER['HTTP_CF_CONNECTING_IP'] : $_SERVER['REMOTE_ADDR'];
-
-//Check if requesting IP is whitelisted
-$check = $ucvs->checkIP($reqIP);
-if($check != true)
-{
-	$ucvs->Log("Wrong IP requested UCVS! - IP: " . $reqIP);
-	die('Wrong IP!');
-}
-else 
-{
-	$result = $ucvs->procData($the_request, $reqIP);
-	if($result !== true)
+	//Check wich request method was used and create a reference
+	switch($_SERVER['REQUEST_METHOD'])
 	{
-		$ucvs->Log("Error while processing data: " . $result . " - User ID: " . $ucvs->getUser($the_request) . " Site: " . $ucvs->getSite($reqIP));
-		echo $result;
+		case 'GET':
+			$the_request = &$_GET;
+		break;
+		
+		case 'POST':
+			$the_request = &$_POST;
+		break;
 	}
-	else
-	{
-		$ucvs->Log("Valid UCVS request finished successfull" . " - User ID: " . $ucvs->getUser($the_request) . " Site: " . $ucvs->getSite($reqIP));
-		echo 'OK';
-	}
-}
 
+	//store requesting IP in a variable | compatibility with CloudFlare
+	$reqIP = isset($_SERVER['HTTP_CF_CONNECTING_IP']) ? $_SERVER['HTTP_CF_CONNECTING_IP'] : $_SERVER['REMOTE_ADDR'];
+
+	//Check if requesting IP is whitelisted
+	$check = $ucvs->checkIP($reqIP);
+	if($check != true)
+	{
+		cLog::Log("Wrong IP requested UCVS! - IP: " . $reqIP);
+		die('Wrong IP!');
+	}
+	else 
+	{
+		$result = $ucvs->procData($the_request, $reqIP);
+		if($result !== true)
+		{
+			cLog::Log("Error while processing data: " . $result . " - User ID: " . $ucvs->getUser($the_request) . " Site: " . $ucvs->getSite($reqIP));
+			echo $result;
+		}
+		else
+		{
+			cLog::Log("Valid UCVS request finished successfull" . " - User ID: " . $ucvs->getUser($the_request) . " Site: " . $ucvs->getSite($reqIP));
+			echo 'OK';
+		}
+	}
 ?>
