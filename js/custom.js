98a2cda7b371fafbe5d3bc5abb00a3fba85dbc0e
Lemoniscool <mrcl.h@gmx.de>
2017-02-06 12:04:36 +0100

    fixed hide/show of CPS settings

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,19 @@
-function cps_switch()
+function cps_hide()
 {
 	var cps_wrapper = document.getElementById('cps_wrapper');
 	
-	if(cps_wrapper.style.display == "none")
-	{
-		cps_wrapper.style.display = "inline";
-	}
-	else
+	if(cps_wrapper.style.display != "none")
 	{
 		cps_wrapper.style.display = "none";
+	}
+}
+
+function cps_show()
+{
+	var cps_wrapper = document.getElementById('cps_wrapper');
+	
+	if(cps_wrapper.style.display != "inline")
+	{
+		cps_wrapper.style.display = "inline";
 	}
 }
