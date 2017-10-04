b4b5e5ac5b211d49dc4c82faf431412bc8653e77
Lemoniscool <mrcl.h@gmx.de>
2017-10-04 15:33:04 +0200

    added comments

--- autoload.php
+++ autoload.php
@@ -1,37 +1,44 @@
 <?php
 
-function autoload($className)
-{
-	//SQL Extension dependant class loading
-	if($className == "cMSSQL")
-	{
-		if(function_exists("mssql_connect"))
-		{
-			$fileName = "cMSSQL.class.php";
-		}
-		else if(function_exists("sqlsrv_connect"))
-		{
-			$fileName = "cMSSQL.SQLSrv.class.php";
-		}
-	}
-	else if($className == "cMySQL")
-	{
-		if(function_exists("mysqli_connect"))
-		{
-			$fileName = "cMySQL.class.php";
-		}
-	}
-	else
-	{
-		$fileName = $className . ".class.php";
-	}
-	
-	
-	if (file_exists("classes/{$fileName}"))
-	{
-		require_once "classes/{$fileName}";
-	}
-}
+	/***************************************/
+	/* UCVS - Unified Callback Vote Script */
+	/*		Written by LemoniscooL		   */
+	/*	 released under GPLv3 license	   */
+	/***************************************/
 
-spl_autoload_register("autoload");
+	function autoload($className)
+	{
+		//SQL Extension dependant class loading
+		if($className == "cMSSQL")
+		{
+			if(function_exists("mssql_connect"))
+			{
+				$fileName = "cMSSQL.class.php";
+			}
+			else if(function_exists("sqlsrv_connect"))
+			{
+				$fileName = "cMSSQL.SQLSrv.class.php";
+			}
+		}
+		else if($className == "cMySQL")
+		{
+			if(function_exists("mysqli_connect"))
+			{
+				$fileName = "cMySQL.class.php";
+			}
+		}
+		else
+		{
+			$fileName = $className . ".class.php";
+		}
+		
+		//require missing classes
+		if (file_exists("classes/{$fileName}"))
+		{
+			require_once "classes/{$fileName}";
+		}
+	}
 
+	spl_autoload_register("autoload");
+
+?>
